099c299f4f4063222f04e766913d0c56449d6a44
George Williams <george.williams@gmail.com>
2013-08-27 18:58:06 -0700

    cms

---
+++ IPAD_PhotoBooth/alpha/PhotomationApp/PhotomationApp/SelectFavoriteHorizViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelectFavoriteHorizViewController.h
+//  PhotomationApp
+//
+//  Created by Cuong George Williams on 6/24/13.
+//  Copyright (c) 2013 Dev Null Enterprises, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectFavoriteHorizViewController : UIViewController
+
+@end
